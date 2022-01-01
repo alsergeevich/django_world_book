f19056b8370b5229a9e40772d9c6a93f6f72e768
alsergeevich <alsergeevich@gmail.com>
2022-01-01 21:59:11 +0500

    Add files

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -125,5 +125,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
